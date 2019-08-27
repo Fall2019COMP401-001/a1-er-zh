dc743f878b6821bdf256a7dac2b2c11150bb2c65
Eric Zheng <erzh@live.unc.edu>
2019-08-27 10:13:58 -0400

    finished novice

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,14 +1,65 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class A1Adept {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int inventorySize, numCustomers, numPurchased;
+		HashMap<String, Double> inventory;
+		HashMap<String, Double> custTotals;
+		String custInfo, fname, lname, fullname;
+		double totalCost;
 		
+		inventorySize = scan.nextInt();
+		scan.nextLine();
+		
+		inventory = new HashMap<String, Double>(inventorySize);
+		
+		for(int i = 0; i < inventorySize; i++) {
+			//gets the item's name and price in a line, splits, then stores that
+			//information in an array
+			String[] itemInfo = scan.nextLine().split("\\s");
+			
+			inventory.put(itemInfo[0], Double.parseDouble(itemInfo[1]));
+		}
+
+		
+		numCustomers = scan.nextInt();
+		scan.nextLine();
+		
+		custTotals = new HashMap<String, Double>(numCustomers);
+		
+		for(int i = 0; i < numCustomers; i++) {
+			custInfo = scan.nextLine();
+			
+			System.out.println(custInfo);
+			
+			Scanner infostr = new Scanner(custInfo);
+			
+			fname = infostr.next();
+			lname = infostr.next();
+			
+			fullname = fname + " " + lname;
+			numPurchased = infostr.nextInt();
+			
+			totalCost = 0.0;
+			
+			for(int j = 0; j < numPurchased; j++) {
+				totalCost += infostr.nextDouble() * inventory.get(infostr.next());
+			}
+			
+			custTotals.put(fullname, totalCost);
+			
+			infostr.close();
+		}
+		
+		System.out.println(inventory.toString());
+		System.out.println(custTotals.toString());
+		
+		scan.close();
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,51 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numCustomers, numPurchased, numItems;
+		double itemCost, totalCost;
+		String fname, lname;
+		String[] customerTotals;
+		
+		//Find the number of customers that the program needs 
+		//to account for
+		numCustomers = scan.nextInt();
+		
+		//create an appropriately sized array to store the information
+		//that will be outputted at the end of the program
+		customerTotals = new String[numCustomers];
+		
+		for(int i = 0; i < numCustomers; i++) {
+			//gets input containing the customer's name and number of items purchased
+		
+			//stores the first letter of the first name
+			fname = Character.toString(scan.next().charAt(0));
+			lname = scan.next();
+			numPurchased = scan.nextInt();
+			
+			totalCost = 0.0;
+			
+			for(int j = 0; j < numPurchased; j++) {
+				//retrieve and store the number and cost of the items purchased
+				numItems = scan.nextInt();
+				//skip over the name of the item since it is not important
+				scan.next();
+				itemCost = scan.nextDouble();
+				
+				totalCost += numItems * itemCost;
+			}
+			
+			customerTotals[i] = fname + ". " + lname + ": " + String.format("%.2f", totalCost);
+			
+		}
+		
+		//print out all of the customer information
+		for(String customer : customerTotals) {
+			System.out.println(customer);
+		}
+		
+		scan.close();
 		
 	}
+	
+	
 }
